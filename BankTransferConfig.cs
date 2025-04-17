5accca69cc3f58299712108d2624fbb746c67db6
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-17 10:58:34 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+class BankTransferConfig
+{
+    public String lang { get; set; }
+    public transfer transfer { get; set; }
+    public String[] methods { get; set; }
+    public confirmation confirmation { get; set; }
+
+    public void read()
+    {
+        String filepath = "bank_transfer_config.json";
+        String configJsonData = File.ReadAllText(filepath);     
+        BankTransferConfig data = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData);
+        this.lang = data.lang;
+        this.transfer = data.transfer;
+        this.methods = data.methods;
+        this.confirmation = data.confirmation;
+
+    }
+
+    public void ubahBahasa(String leng, BankTransferConfig data)
+    {
+        String filepath = "bank_transfer_config.json";
+        if (lang == "en")
+        {
+            data.lang = "id";
+        }
+        else
+        {
+            data.lang = "en";
+        }
+        data.lang = lang;
+        String json = JsonSerializer.Serialize(data);
+        
+        
+        File.WriteAllText(filepath, json);
+    }
+}
+
+class transfer
+{
+    public int threshold { get; set; }
+    public int low_fee { get; set; }
+    public int high_fee { get; set; }
+}
+
+class confirmation
+{
+    public String en { get; set; }
+    public String id { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        config.read();
+        config.ubahBahasa(config.lang, config);
+        if (config.lang == "en")
+        {
+            Console.WriteLine("Current Language: English");
+            Console.WriteLine("Transfer: ");
+            int uang = int.Parse(Console.ReadLine());
+            int totalBiaya;
+            if(uang < config.transfer.threshold)
+            {
+                totalBiaya = uang+ config.transfer.low_fee;
+                Console.WriteLine("Low fee: " + config.transfer.low_fee);
+            }
+            else
+            {
+                totalBiaya = uang + config.transfer.high_fee;
+                Console.WriteLine("High fee: " + config.transfer.high_fee);
+            }
+            Console.WriteLine("Total amount= " + totalBiaya);
+            Console.WriteLine("Select transfer method:");
+            for (int i = 0; i < config.methods.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + config.methods[i]);
+            }
+            Console.WriteLine("Please type "+config.confirmation.en+ "to confirm the transaction");
+            String conf = Console.ReadLine();
+            if (conf == config.confirmation.en)
+            {
+                Console.WriteLine("transfer is complete");
+            }else
+            {
+                Console.WriteLine("transfer is cancelled");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Current Language: Indonesian");
+            Console.WriteLine("Transfer: ");
+            int uang = int.Parse(Console.ReadLine());
+            int totalBiaya;
+            if (uang < config.transfer.threshold)
+            {
+                totalBiaya = uang + config.transfer.low_fee;
+                Console.WriteLine("Low fee: " + config.transfer.low_fee);
+            }
+            else
+            {
+                totalBiaya = uang + config.transfer.high_fee;
+                Console.WriteLine("High fee: " + config.transfer.high_fee);
+            }
+            Console.WriteLine("Total amount= " + totalBiaya);
+            Console.WriteLine("Pilih metode transfer");
+            for (int i = 0; i < config.methods.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + config.methods[i]);
+            }
+            Console.WriteLine("Ketik " + config.confirmation.en + " untuk mengonfirmasi transaksi");
+            String conf = Console.ReadLine();
+            if (conf == config.confirmation.en)
+            {
+                Console.WriteLine(" Proses transfer berhasil");
+            }
+            else
+            {
+                Console.WriteLine(" Transfer dibatalkan");
+            }
+        }
+    }
+}
